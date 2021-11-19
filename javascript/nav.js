d5204d633e0f1d05f5f759be824c52acf37de20b
davif <flipedavi80@gmail.com>
2021-11-18 22:47:05 -0300

    commit de projeto refeito

---
+++ javascript/nav.js
@@ -0,0 +1,13 @@
+$('.btn').click(function() {
+    $('.btn').toggleClass("click");
+    $('.sidebar').toggleClass("show");
+});
+
+$('.relv-btn').click(function() {
+    $('nav ul .relv-mostrar').toggleClass("show");
+    $('nav ul .relv').toggleClass("rotate");
+});
+
+$('nav ul li').click(function() {
+    $(this).addClass("active").siblings().removeClass("active");
+});

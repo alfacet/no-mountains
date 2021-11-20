686c6031cfce25e36e50a5eded9efe5462d5d8fb
davif <flipedavi80@gmail.com>
2021-11-20 00:10:38 -0300

    ajeitadinha

--- nav.js
+++ nav.js
@@ -1,13 +1,13 @@
-$('.btn').click(function() {
-    $('.btn').toggleClass("click");
-    $('.sidebar').toggleClass("show");
-});
+    $('.btn').click(function() {
+        $('.btn').toggleClass("click");
+        $('.sidebar').toggleClass("show");
+    });
 
-$('.relv-btn').click(function() {
-    $('nav ul .relv-mostrar').toggleClass("show");
-    $('nav ul .relv').toggleClass("rotate");
-});
+    $('.relv-btn').click(function() {
+        $('nav ul .relv-mostrar').toggleClass("show");
+        $('nav ul .relv').toggleClass("rotate");
+    });
 
-$('nav ul li').click(function() {
-    $(this).addClass("active").siblings().removeClass("active");
-});
+    $('nav ul li').click(function() {
+        $(this).addClass("active").siblings().removeClass("active");
+    });

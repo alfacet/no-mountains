cc1adddb3f57c5d39b7b807271777c121fc7551a
davif <flipedavi80@gmail.com>
2021-11-20 00:32:22 -0300

    last

--- nav.js
+++ nav.js
@@ -3,11 +3,17 @@
         $('.sidebar').toggleClass("show");
     });
 
+    /*Parte Um*/
+
     $('.relv-btn').click(function() {
         $('nav ul .relv-mostrar').toggleClass("show");
         $('nav ul .relv').toggleClass("rotate");
     });
 
+    /*Parte Dois*/
+
     $('nav ul li').click(function() {
         $(this).addClass("active").siblings().removeClass("active");
     });
+
+    /*Parte Três*/
